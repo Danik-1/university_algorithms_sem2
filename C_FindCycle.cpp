f4115141caae1e2772f0a0c159619864f71f2798
Danik-1 <lyatorovskiy@gmail.com>
2023-02-15 13:53:23 +0300

    Written first iteration of C task Wrong Answer on 3rd test

--- C_FindCycle.cpp
+++ C_FindCycle.cpp
@@ -1,12 +1,103 @@
-/*
- *   Copyright (c) 2023 
- *   All rights reserved.
- */
-
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+
+template <typename Vertex, typename Edge = std::pair<Vertex, Vertex>>
+class AbstractGraph {
+ public:
+  using VertexType = Vertex;
+  using EdgeType = Edge;
+
+  explicit AbstractGraph(size_t vertices_num, size_t edges_num = 0) :
+      vertices_number_(vertices_num), edges_number_(edges_num) {}
+
+  size_t GetVerticesNumber() const { return vertices_number_; }
+
+  virtual std::vector<Vertex> GetNeighbours(const Vertex& vertex) = 0;
+ protected:
+  size_t vertices_number_ = 0;
+  size_t edges_number_ = 0;
+};
+
+template <typename Vertex, typename Edge = std::pair<Vertex, Vertex>>
+class ListGraph : public AbstractGraph<Vertex, Edge> {
+ public:
+  ListGraph(size_t vertices_num, const std::vector<Edge>& edges) :
+      AbstractGraph<Vertex, Edge>(vertices_num, edges.size()) {
+    for (const auto& edge : edges)
+      list_[edge.first].push_back(edge.second);
+  }
+
+  std::vector<Vertex> GetNeighbours(const Vertex& vertex) final {
+    return list_[vertex];
+  }
+
+ private:
+  std::unordered_map<Vertex, std::vector<Vertex>> list_;
+};
+
+
+template <typename T = size_t>
+std::vector<std::pair<T, T>> GetEdges(const size_t& edges_number) {
+  std::vector<std::pair<size_t, size_t>> edges(edges_number);
+  for (size_t i = 0; i < edges_number; ++i)
+    std::cin >> edges[i].first >> edges[i].second;
+
+  return edges;
+}
+
+enum Color {
+  white,
+  grey,
+  black
+};
+
+template <typename Graph, typename VType = typename Graph::VertexType>
+bool FindCycleDFS(VType& from, Graph* graph,
+                  std::unordered_map<VType, Color>& colors,
+                  std::vector<VType>* parents) {
+  colors[from] = grey;
+  for (auto v : graph->GetNeighbours(from)) {
+    if ((colors[v] == white && FindCycleDFS(v, graph, colors, parents))
+        || colors[v] == grey) {
+      parents->push_back(v);
+      return true;
+    }
+  }
+  parents->pop_back();
+  colors[from] = black;
+  return false;
+}
+
+template <typename Graph, typename VType = typename Graph::VertexType>
+std::vector<VType> GetCycle(Graph* graph) {
+  std::unordered_map<VType, Color> colors;
+  std::vector<VType> cycle{};
+
+  for (size_t i = 1; i <= graph->GetVerticesNumber(); ++i)
+    if (colors[i] == white && FindCycleDFS(i, graph, colors, &cycle))
+      return cycle;
+  return {};
+}
+
+template <typename VType = size_t>
+void PrintCycle(const std::vector<VType>& cycle) {
+  if (cycle.empty()) {
+    std::cout << "NO";
+  } else {
+    std::cout << "YES" << "\n";
+    for (size_t j = 0; j < cycle.size(); ++j)
+      std::cout << cycle[j] << " ";
+  }
+}
+
 
 int main() {
-  uint32_t vertexNumber;
-  uint32_t edgesNumber;
-  std::cin >> vertexNumber >> edgesNumber;
+  size_t vertecies_number = 0;
+  size_t edges_number = 0;
+  std::cin >> vertecies_number >> edges_number;
+
+  ListGraph<size_t> graph(vertecies_number, GetEdges<size_t>(edges_number));
+  PrintCycle(GetCycle(&graph));
 }
