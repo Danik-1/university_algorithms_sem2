08b0c1ea6d1aadb8a2912b5eda8deeb8e0bd4a10
Danik-1 <lyatorovskiy@gmail.com>
2023-03-05 15:58:02 +0300

    Task G done on OK

--- G_FloydWarshall.cpp
+++ G_FloydWarshall.cpp
@@ -3,13 +3,13 @@
 #include <unordered_map>
 #include <vector>
 
-template <typename EType = bool,
+template <typename EType = int32_t,
           typename Matrix = std::vector<std::vector<EType>>>
-Matrix ReadEdgesFromMatrix(const int8_t& number_of_vertexes) {
-  Matrix edges;
-  for (int8_t to = 0; to < number_of_vertexes; ++to) {
-    for (int8_t from = 0; from < number_of_vertexes; ++from) {
-      bool edge;
+Matrix ReadEdgesFromMatrix(const int32_t& number_of_vertexes) {
+  Matrix edges(number_of_vertexes, std::vector<EType>(number_of_vertexes, 0));
+  int32_t edge = 0;
+  for (int32_t from = 0; from < number_of_vertexes; ++from) {
+    for (int32_t to = 0; to < number_of_vertexes; ++to) {
       std::cin >> edge;
       edges[from][to] = edge;
     }
@@ -18,19 +18,22 @@ Matrix ReadEdgesFromMatrix(const int8_t& number_of_vertexes) {
   return edges;
 }
 
-template <typename EType = bool,
+template <typename EType = int32_t,
           typename Matrix = std::vector<std::vector<EType>>>
 Matrix FloydWarshall(Matrix edges) {
-  int8_t number_of_vertexes = edges.size();
-  for (int8_t k = 0; k < number_of_vertexes; ++k)
-    for (int8_t u = 0; u < number_of_vertexes; ++u)
-      for (int8_t v = 0; v < number_of_vertexes; ++v)
-        edges[u][v] = edges[u][v] || (edges[u][k] + edges[k][v]);
+  int32_t number_of_vertexes = edges.size();
+  for (int32_t k = 0; k < number_of_vertexes; ++k) {
+    for (int32_t u = 0; u < number_of_vertexes; ++u) {
+      for (int32_t v = 0; v < number_of_vertexes; ++v) {
+        edges[u][v] = edges[u][v] || (edges[u][k] && edges[k][v]);
+      }
+    }
+  }
 
   return edges;
 }
 
-template <typename T = int8_t>
+template <typename T = int32_t>
 void PrintMatrix(const std::vector<std::vector<T>>& matrix) {
   for (const auto& row : matrix) {
     for (const auto& element : row) {
@@ -41,12 +44,14 @@ void PrintMatrix(const std::vector<std::vector<T>>& matrix) {
 }
 
 int main() {
-  int8_t number_of_vertexes = 0;
+  int32_t number_of_vertexes = 0;
   std::cin >> number_of_vertexes;
 
-  std::vector<std::vector<bool>> matrixGraph = ReadEdgesFromMatrix(number_of_vertexes);
-  std::vector<std::vector<bool>> allPaths = FloydWarshall(matrixGraph);
-  PrintMatrix(allPaths);
+  using Matrix = std::vector<std::vector<int32_t>>;
+  Matrix matrix_graph = ReadEdgesFromMatrix(number_of_vertexes);
+  Matrix all_paths = FloydWarshall(matrix_graph);
+
+  PrintMatrix(all_paths);
 
   return 0;
 }
